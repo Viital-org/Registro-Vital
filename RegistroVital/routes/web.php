312806477a7a510ba850e6a9c30ab552602099e5
Luanzinho <luangcubas@gmail.com>
2024-04-23 21:41:15 -0300

    Criada tela de cadastro basica e a rota para ela

--- RegistroVital/routes/web.php
+++ RegistroVital/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastroprofissional', function () {
+    return view('cadastroprofissional');
+});
