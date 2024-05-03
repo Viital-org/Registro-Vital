baeb8be8825570a6babe960e13c0e064593f99dd
Luanzinho <luangcubas@gmail.com>
2024-05-02 22:39:38 -0300

    Telas de confirmacao para exclusao em todas as classes

--- RegistroVital/resources/views/Cadastros/listadicas.blade.php
+++ RegistroVital/resources/views/Cadastros/listadicas.blade.php
@@ -41,10 +41,8 @@
                         </svg>
                     </a></th>
                 <th>
-                    <form action="{{ route('dicas-delete', ['id'=> $item->id])}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">
+
+                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#delete">
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20"
                                  viewBox="0 0 24 24">
                                 <path
@@ -52,6 +50,31 @@
                                 </path>
                             </svg>
                         </button>
+
+                    <form action="{{ route('dicas-delete', ['id'=> $item->id])}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                                    <!-- Modal -->
+                        <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Confirmacao de exclusao</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Deseja realmente excluir o registro {{$item->id}} ?  <p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-primary">Excluir</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        
                     </form>
             </tr>
         @endforeach
