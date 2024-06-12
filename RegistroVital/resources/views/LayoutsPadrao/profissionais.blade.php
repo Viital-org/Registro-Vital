b725d8b2b5dbc22fa9b6a6b8753820aaea35c955
Luan Cubas <luangcubas@gmail.com>
2024-06-12 16:00:49 -0700

    Layout base da barra lateral e superior

--- RegistroVital/resources/views/LayoutsPadrao/profissionais.blade.php
+++ RegistroVital/resources/views/LayoutsPadrao/profissionais.blade.php
@@ -9,9 +9,55 @@
 
     <!-- Importação de arquivos JavaScript e CSS com Vite -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="{{ asset('resources/sass/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <!-- Conteúdo da página -->
-@yield('conteudo')
+<div class="container-fluid">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Registro Vital</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quem Somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Help</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Conteúdo principal com sidebar -->
+    <div class="row">
+        <!-- Sidebar -->
+        <div class="col-md-3 bg-light sidebar">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 3</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Conteúdo principal -->
+        <div class="col-md-9 ml-sm-auto col-lg-9 pt-3 px-4 main-content">
+            @yield('conteudo')
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
