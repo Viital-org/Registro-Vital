8f49911ec9d42f37659b2fd8f30bbe1de233ec9c
GabrielTurcato <gatuviso@gmail.com>
2024-06-25 19:46:40 -0300

    Login e Registro

---
+++ RegistroVital/resources/views/Auth/forgot-password.blade.php
@@ -0,0 +1,28 @@
+@extends ('LayoutsPadrao.inicio')
+
+@section('titulo', 'Esqueci minha senha')
+
+@section('conteudo')
+    @if (session('status'))
+        <div class="alert alert-success" role="alert"> {{ session('status') }}</div>
+    @endif
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+            <h1>Recuperar senha</h1>
+            <div>
+                <label for="email" class="form-label">Endereço de Email</label>
+                <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+            </div>
+
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Enviar email de recuperação da senha</button>
+            </div>
+@endsection
+
+
--- RegistroVital/resources/views/Cadastros/listaagendamentopaciente.blade.php
+++ RegistroVital/resources/views/Cadastros/listaagendamentopaciente.blade.php
@@ -4,21 +4,21 @@
 
 @section('conteudo')
 
-@csrf
-&nbsp;
+    @csrf
+    &nbsp;
 
-<h1>Consultas de agendamentos</h1>
+    <h1>Consultas de agendamentos</h1>
 
-<nav class="d-flex align-items-center justify-content-between" role="search">
-    <form action="{{ route('agendamentos-paciente-show') }}" method="post" class="d-flex w-100">
-        @csrf
-        <input type="date" name="" id="" class="form-control me-2 flex-grow-1">
-        <button class="btn btn-light" type="submit">Buscar</button>
-    </form>
-</nav>
+    <nav class="d-flex align-items-center justify-content-between" role="search">
+        <form action="{{ route('agendamentos-paciente-show') }}" method="post" class="d-flex w-100">
+            @csrf
+            <input type="date" name="" id="" class="form-control me-2 flex-grow-1">
+            <button class="btn btn-light" type="submit">Buscar</button>
+        </form>
+    </nav>
 
-<table class="table">
-    <thead>
+    <table class="table">
+        <thead>
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Profissional</th>
@@ -26,17 +26,17 @@
             <th scope="col">Código da consulta</th>
             <th scope="col">Data</th>
         </tr>
-    </thead>
-    <tbody>
-    @foreach ($agendamento_paciente as $item)
-        <tr>
-            <th scope="row">{{$item->id}}</th>
-            <td>{{$item->nome_profissional ?: 'Não definido' }}</td>
-            <td>{{$item->tipo_especializacao}}</td>
-            <td>{{$item->consulta_id}}</td>
-            <td>{{$item->data_consulta}}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        @foreach ($agendamento_paciente as $item)
+            <tr>
+                <th scope="row">{{$item->id}}</th>
+                <td>{{$item->nome_profissional ?: 'Não definido' }}</td>
+                <td>{{$item->tipo_especializacao}}</td>
+                <td>{{$item->consulta_id}}</td>
+                <td>{{$item->data_consulta}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
