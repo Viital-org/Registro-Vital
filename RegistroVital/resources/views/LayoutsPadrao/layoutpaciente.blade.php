7859358884bd4caa9253061e87fc7532a7d91cbf
Bruno Marques <brunofmarques10@gmail.com>
2024-06-27 22:02:33 -0300

    Logo

--- RegistroVital/resources/views/LayoutsPadrao/layoutpaciente.blade.php
+++ RegistroVital/resources/views/LayoutsPadrao/layoutpaciente.blade.php
@@ -20,6 +20,7 @@
     <div class="row">
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <img src="{{ asset('/img/cruz.png') }}" style="max-width:3%; margin-left:1%"/>
             <a class="navbar-brand" href="/">Registro Vital</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
