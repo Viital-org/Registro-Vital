15ec387937cd3db263656f554e79bffa98e63b4c
Bruno Marques <brunofmarques10@gmail.com>
2024-10-25 22:09:39 -0300

    #35 - Alteração identidade visual

--- RegistroVital/resources/views/LayoutsPadrao/inicio.blade.php
+++ RegistroVital/resources/views/LayoutsPadrao/inicio.blade.php
@@ -14,41 +14,112 @@
 </head>
 <body>
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg fixed-top" style="background-color: #4a6572; color: white;">
-    <div class="container">
-        <img src="{{ asset('/img/cruz.png') }}" alt="Logo" style="max-width: 3%;"/>
-        <a class="navbar-brand" href="/" style="color: white; font-weight: bold; font-size: 20pt;">Registro Vital</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon" style="color: white;"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    @if (auth()->check())
-                        <a class="nav-link"
-                           href="{{ auth()->usuario()->tipo_usuario === '1' ? route('paciente.dashboard') : route('profissional.dashboard') }}"
-                           style="color: white;">
-                            Home
-                        </a>
-                    @endif
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('quemsomos') }}" style="color: white;">Quem somos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('ajuda') }}" style="color: white;">Ajuda</a>
-                </li>
-            </ul>
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <img src="{{ asset('/img/cruz.png') }}" alt="Logo" style="max-width: 3%;"/>
+            <a class="navbar-brand" href="/">Registro Vital</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        @if (auth()->check())
+                            <a class="nav-link"
+                            href="{{ auth()->usuario()->tipo_usuario === '1' ? route('paciente.dashboard') : route('profissional.dashboard') }}"
+                            style="color: white;">
+                                Home
+                            </a>
+                        @endif
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('quemsomos') }}" style="color: white;">Quem somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('ajuda') }}" style="color: white;">Ajuda</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</header>
 
 <!-- Conteúdo da página -->
 <div class="container-fluid conteudo-ajuda mt-5 pt-5" style="background-color: #d4e6e5; min-height: 100vh;">
     @yield('conteudo')
 </div>
+
+                <!-- Formulário de Login -->
+                <div class="card shadow-sm p-4">
+                    <h1 class="">Login</h1>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <!-- Campo de E-mail -->
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Endereço de E-Mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                class="form-control @error('email') is-invalid @enderror"
+                                value="{{ old('email') }}"
+                                required
+                                autocomplete="email"
+                                autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                            @enderror
+                        </div>
+
+                        <!-- Campo de Senha -->
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Senha</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                required
+                                autocomplete="current-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                            @enderror
+                        </div>
+
+                        <!-- Opção de "Lembre-se de mim" -->
+                        <div class="form-check mb-3">
+                            <input
+                                type="checkbox"
+                                name="remember"
+                                class="form-check-input"
+                                id="remember"
+                                {{ old('remember') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="remember">Lembre-se de mim</label>
+                        </div>
+
+                        <!-- Botões -->
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary btn-lg">Entrar</button>
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link text-center" href="{{ route('password.request') }}">Esqueci minha
+                                    senha</a>
+                            @endif
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
