d408f72091ee01441b0a4a1babce6fa1a8e900ec
Bruno Marques <brunofmarques10@gmail.com>
2024-10-29 00:06:02 -0300

    #46 - Correção de merge (view profissionais)

--- RegistroVital/resources/views/LayoutsPadrao/layoutadministrador.blade.php
+++ RegistroVital/resources/views/LayoutsPadrao/layoutadministrador.blade.php
@@ -11,79 +11,14 @@
 </head>
 <body>
 
-<<<<<<< HEAD
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <img src="{{ asset('/img/cruz.png') }}" alt="Logo" class="img-fluid"
-                 style="max-width: 30px; margin-right: 10px;">
-            Registro Vital - Admin
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('administrador.dashboard') }}">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('quemsomos') }}">Quem somos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('ajuda') }}">Ajuda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('administrador.profissionais') }}">Profissionais</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('relatorios_administrador') }}">Relatórios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('administrador.logs') }}">Logs</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    @auth
-                        <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
-                           data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->nome_completo }}</a>
-                        <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                            <li>
-                                <form method="GET" action="{{ route('profile.edit') }}" id="edit-form">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">Editar Perfil</button>
-                                </form>
-                            </li>
-                            <li>
-                                <form method="POST" action="{{ route('logout') }}" id="logout-form">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">Sair</button>
-                                </form>
-                            </li>
-                        </ul>
-                    @else
-                        <a class="nav-link" href="{{ route('login') }}">Entrar</a>
-                    @endauth
-                </li>
-            </ul>
-=======
     <!-- Sidebar -->
     <aside class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar sidebar-admin">
         <div class="d-flex flex-column align-items-center sidebar-header p-4">
             <img src="{{ asset('/img/cruz.png') }}" alt="Logo Registro Vital" class="logo img-fluid mb-2">
             <h3 class="titulo text-white">Registro Vital</h3>
             <h5 class="titulo">Administrador</h5>
->>>>>>> Bruno
         </div>
 
-<<<<<<< HEAD
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-=======
         <ul class="nav flex-column links">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('administrador.dashboard') }}">
@@ -114,6 +49,9 @@
                     <i class="fas fa-history"></i> 
                     <p class="p-nav">Logs</p>
                 </a>
+            </li>
+            <li class="nav-item">
+                    <a class="nav-link" href="{{ route('administrador.profissionais') }}">Profissionais</a>
             </li>
         </ul>
 
@@ -150,6 +88,5 @@
         @yield('conteudo')
     </main>
 
->>>>>>> Bruno
 </body>
 </html>
