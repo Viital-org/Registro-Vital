70c578f109ca4c9586b2cc3a69280e2da1734a6e
Bruno Marques <brunofmarques10@gmail.com>
2024-10-31 19:37:56 -0300

    #49 - Modificação tela inicial e sidebar administrador

--- RegistroVital/resources/views/LayoutsPadrao/layoutadministrador.blade.php
+++ RegistroVital/resources/views/LayoutsPadrao/layoutadministrador.blade.php
@@ -12,7 +12,7 @@
 <body>
 
     <!-- Sidebar -->
-    <aside class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar sidebar-admin">
+    <aside class="bg-gradient-primary sidebar-admin sidebar-dark accordion" id="accordionSidebar sidebar-admin">
         <div class="d-flex flex-column align-items-center sidebar-header p-4">
             <img src="{{ asset('/img/cruz.png') }}" alt="Logo Registro Vital" class="logo img-fluid mb-2">
             <h3 class="titulo text-white">Registro Vital</h3>
@@ -21,72 +21,54 @@
 
         <ul class="nav flex-column links">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('administrador.dashboard') }}">
-                    <i class="fas fa-tachometer-alt"></i> 
-                    <p class="p-nav">Dashboard</p>
-                </a>
+                <a class="nav-link" href="{{ route('administrador.dashboard') }}">Dashboard</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('quemsomos') }}">
-                    <i class="fas fa-users"></i>
-                    <p class="p-nav">Quem somos</p>
-                </a>
+                <a class="nav-link" href="{{ route('quemsomos') }}">Quem Somos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('ajuda') }}">
-                    <i class="fas fa-question-circle"></i> 
-                    <p class="p-nav">Ajuda</p>
-                </a>
+                <a class="nav-link" href="{{ route('ajuda') }}">Ajuda</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('relatorios_administrador') }}">
-                    <i class="fas fa-file-alt"></i> 
-                    <p class="p-nav">Relatórios</p>
-                </a>
+                <a class="nav-link" href="{{ route('relatorios_administrador') }}">Relatórios</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('administrador.logs') }}">
-                    <i class="fas fa-history"></i> 
-                    <p class="p-nav">Logs</p>
-                </a>
+                <a class="nav-link" href="{{ route('administrador.logs') }}">Logs</a>
             </li>
             <li class="nav-item">
                     <a class="nav-link" href="{{ route('administrador.profissionais') }}">Profissionais</a>
             </li>
         </ul>
-
-        <ul class="perfil nav flex-column links">
-                <!-- Perfil Usuário -->
-                <li class="user nav-item dropdown no-arrow">
-                @auth
-                    <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="{{ asset('/img/avatar.png') }}" alt="Avatar do usuário" class="userImg img-fluid rounded-circle ms-2">
-                        <span>{{ Auth::user()->nome_completo }}</span>
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                        <li>
-                            <a href="{{ route('profile.edit') }}" class="dropdown-item">Editar Perfil</a>
-                        </li>
-                        <li>
-                            <form method="POST" action="{{ route('logout') }}" id="logout-form">
-                                @csrf
-                                <button type="submit" class="dropdown-item">Sair</button>
-                            </form>
-                        </li>
-                    </ul>
-                @else
-                    <a class="nav-link" href="{{ route('login') }}">
-                        <i class="fas fa-sign-in-alt"></i> Entrar
-                    </a>
-                @endauth
-            </li>
-        </ul>
+        <div class="perfil">
+            @auth
+                <a class="nav-link" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="{{ asset('/img/avatar.png') }}" alt="Avatar do usuário" class="userImg img-fluid rounded-circle ms-2">
+                    <span>{{ Auth::user()->nome_completo }}</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="profileDropdown">
+                    <li>
+                        <a href="{{ route('profile.edit') }}" class="dropdown-item">Editar Perfil</a>
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}" id="logout-form">
+                            @csrf
+                            <button type="submit" class="dropdown-item">Sair</button>
+                        </form>
+                    </li>
+                </ul>
+            @else
+                <a class="nav-link" href="{{ route('login') }}">
+                    <i class="fas fa-sign-in-alt"></i> Entrar
+                </a>
+            @endauth
+        </div>
     </aside>
 
     <!-- Conteúdo Principal -->
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-content">
-        @yield('conteudo')
-    </main>
-
+    <div class="container">
+        <main class="main-content">
+            @yield('conteudo')
+        </main>
+    </div>
 </body>
 </html>
