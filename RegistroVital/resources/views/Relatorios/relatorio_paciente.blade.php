76a7a4c45acfe9a5d38dc6b564d105211550ca0f
Bruno Marques <brunofmarques10@gmail.com>
2024-11-17 17:10:09 -0300

    #85 - Arquivo faltante

--- RegistroVital/resources/views/Relatorios/relatorio_paciente.blade.php
+++ RegistroVital/resources/views/Relatorios/relatorio_paciente.blade.php
@@ -61,7 +61,7 @@
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">sdnjcnskjv</div>
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">6</div>
                             </div>  
                         </div>
                     </div>
